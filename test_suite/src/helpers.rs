80608e52bb7be2c90b90a984256f0edaae478367
Serhii Potapov <blake131313@gmail.com>
2025-04-15 22:34:39 +0200

    Fix test on CI

--- test_suite/src/helpers.rs
+++ test_suite/src/helpers.rs
@@ -1,10 +1,13 @@
 use crate::database::Database;
 use uuid::Uuid;
 
+/// Directory with test data files.
+const DATA_DIR: &str = "data";
+
 /// Generate a unique file path for the database file.
 pub fn gen_db_file_path() -> String {
     let id = Uuid::new_v4();
-    format!("db/test-{}.json", id)
+    format!("{DATA_DIR}/test-{}.json", id)
 }
 
 /// Open a database and pass it to a closure.
@@ -13,6 +16,10 @@ pub fn with_open_db<F>(f: F)
 where
     F: FnOnce(Database),
 {
+    if !std::path::Path::new(DATA_DIR).exists() {
+        std::fs::create_dir_all(DATA_DIR).unwrap();
+    }
+
     let file_path = gen_db_file_path();
     let db = Database::open(&file_path).unwrap();
     f(db);
